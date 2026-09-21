49294ae312716804a0c5402c56374db08307b166
Molina <322942151+MS-SaaS@users.noreply.github.com>
2026-09-20 21:02:23 -0300

    docs: alterção no README.md

--- consumo-agua/app.py
+++ consumo-agua/app.py
@@ -3,5 +3,8 @@ consumo = float(input("Digite o consumo de água em metros cúbicos: "))
 
 if tipo_de_local == "comercial":
     print("Tarifa comercial aplicada — consulte o plano corporativo.")
-elif tipo_de_local == "apartamento": and consumo < 10:
-          
+elif tipo_de_local == "apartamento" and consumo < 10:
+   print("Consumo econômico! Excelente controle de água.")
+elif tipo_de_local == "apartamento" or "casa" and consumo <= 25:
+   print("Consumo moderado — dentro do padrão residencial.")
+else: print ("Consumo excessivo — adote medidas de economia e verifique vazamentos.")
